b5f19ed4c45cc9ce5461b8e86f2799dad289da74
Kevin Santo Cappuccio <arabidsquid@gmail.com>
2023-03-25 13:01:09 -0700

    Add files via upload

---
+++ code/JumperlessDefines.h
@@ -0,0 +1,143 @@
+
+#define CHIP_A 0
+#define CHIP_B 1
+#define CHIP_C 2
+#define CHIP_D 3
+#define CHIP_E 4
+#define CHIP_F 5
+#define CHIP_G 6
+#define CHIP_H 7
+#define CHIP_I 8
+#define CHIP_J 9
+#define CHIP_K 10
+
+#define CS_A PIN_PA4
+#define CS_B PIN_PA5
+#define CS_C PIN_PA6
+#define CS_D PIN_PA7
+#define CS_E PIN_PF3
+#define CS_F PIN_PF4
+#define CS_G PIN_PF5
+#define CS_H PIN_PD1
+#define CS_I PIN_PD2
+#define CS_J PIN_PD3
+#define CS_K PIN_PD4
+
+#define DATAPIN PIN_PA0
+#define RESET PIN_PD7
+#define STROBE PIN_PA1
+
+#define AX0 PIN_PC0
+#define AX1 PIN_PC1
+#define AX2 PIN_PC2
+#define AX3 PIN_PC3
+
+#define AY0 PIN_PF0
+#define AY1 PIN_PF1
+#define AY2 PIN_PF2
+
+#define DAC_OUT PIN_PD6  
+//#define I2C_UART_SELECTION_IN PIN_PD7  
+
+#define IN_TX_SDA PIN_PA2
+#define IN_RX_SCL PIN_PA3
+
+#define t1  1
+#define t2  2
+#define t3  3
+#define t4  4
+#define t5  5
+#define t6  6
+#define t7  7
+#define t8  8
+#define t9  9
+#define t10 10
+#define t11 11
+#define t12 12
+#define t13 13
+#define t14 14
+#define t15 15
+#define t16 16
+#define t17 17
+#define t18 18
+#define t19 19
+#define t20 20
+#define t21 21
+#define t22 22
+#define t23 23
+#define t24 24
+#define t25 25
+#define t26 26
+#define t27 27
+#define t28 28
+#define t29 29
+#define t30 30
+
+
+#define b1  32
+#define b2  33
+#define b3  34
+#define b4  35
+#define b5  36
+#define b6  37
+#define b7  38
+#define b8  39
+#define b9  40
+#define b10 41
+#define b11 42
+#define b12 43
+#define b13 44
+#define b14 45
+#define b15 46
+#define b16 47
+#define b17 48
+#define b18 49
+#define b19 50
+#define b20 51
+#define b21 52
+#define b22 53
+#define b23 54
+#define b24 55
+#define b25 56
+#define b26 57
+#define b27 58
+#define b28 59
+#define b29 60
+#define b30 61
+
+
+
+
+#define NANO_D0  70 //these are completely arbitrary but they should come in handy
+#define NANO_D1  71 
+#define NANO_D2  72 
+#define NANO_D3  73 
+#define NANO_D4  74 
+#define NANO_D5  75 
+#define NANO_D6  76 
+#define NANO_D7  77 
+#define NANO_D8  78 
+#define NANO_D9  79 
+#define NANO_D10  80 
+#define NANO_D11  81 
+#define NANO_D12  82 
+#define NANO_D13  83 
+#define NANO_RESET  84 
+#define NANO_AREF  85 
+#define NANO_A0  86 
+#define NANO_A1  87 
+#define NANO_A2  88 
+#define NANO_A3  89 
+#define NANO_A4  90 
+#define NANO_A5  91 
+#define NANO_A6  92 
+#define NANO_A7  93
+
+
+#define GND  100 
+#define TOP_RAIL  101 
+#define BOTTOM_RAIL  102 
+#define DAC0TO5V  103 
+#define DACPLUSMINUS9V  104
+
+
